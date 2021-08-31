e65570ee751193159d7278ac1d8134ff12807278
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-08-31 14:39:01 -0400

    updated package,json

---
+++ client/build/precache-manifest.2adfc16e6ec6f077a46ea5112e302b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b058ec9f470a340f079da2d2b710cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7662d280a7d748b3b3b8",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "1ea05d3f99728d6944fd",
+    "url": "/static/js/2.4c5f5301.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.4c5f5301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7662d280a7d748b3b3b8",
+    "url": "/static/js/main.a2a57a1f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
